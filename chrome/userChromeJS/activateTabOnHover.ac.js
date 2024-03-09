1db6dd0af7af98459e96590a9c56363c78b7bda6
Ryan Lieu <github-benzBrake@woai.ru>
2024-03-09 15:04:57 +0800

    feat(JS): activateTabOnHover.ac.js 自动切换支持标签分组

--- chrome/userChromeJS/activateTabOnHover.ac.js
+++ chrome/userChromeJS/activateTabOnHover.ac.js
@@ -1,9 +1,28 @@
-// Activate Tab On Hover
-// version 2022.10.0
-// https://forum.vivaldi.net/post/395460
-// Activates tab on hover.
-
+// ==UserScript==
+// @name            activateTabOnHover.uc.js
+// @description     激活鼠标指向标签页
+// @license         MIT License
+// @compatibility   Vivaldi 6
+// @version         20240309
+// @charset         UTF-8
+// @homepageURL     https://github.com/benzBrake/VivaldiMods/tree/main/chrome/userChromeJS
+// @note            源自 https://forum.vivaldi.net/post/395460，修改兼容标签分组
+// ==/UserScript==
 (function activateTab() {
+  let activateTabForGroups = {}; // 不知道怎么从 Vivaldi 获取当前标签分组最后激活的标签就记录起来
+  async function swithToTab(id) {
+    let tabs = await chrome.tabs.query({ currentWindow: true });
+    tabs = tabs.filter((tab) => tab.id === id);
+    if (tabs.length > 0) {
+      let { vivExtData } = tabs[0];
+      let extDataObj = JSON.parse(vivExtData);
+      if ("group" in extDataObj) {
+        // 记录当前标签分组最后激活的标签
+        activateTabForGroups[extDataObj.group] = id;
+      }
+      chrome.tabs.update(id, { active: true });
+    }
+  }
   function hover(e, tab) {
     if (
       !tab.parentNode.classList.contains("active") &&
@@ -16,17 +35,32 @@
       });
       wait = setTimeout(async function () {
         if (tab.parentNode.parentNode.classList.contains("is-substack")) {
-          let groupId = tab.parentNode.id.replace(/^tab-/g, "");
+          // 指向的是标签分组
+          const groupId = tab.parentNode.id.replace(/^tab-/g, "");
           let tabs = await chrome.tabs.query({ currentWindow: true });
           tabs = tabs.filter(t => {
             return JSON.parse(t.vivExtData).group == groupId;
           });
-          tabs.forEach(t => {
-            // 待完成标签组切换
-          })
+          if (tabs.length === 0) return;
+          if (groupId in activateTabForGroups) {
+            const filteredTabs = tabs.filter(t => {
+              return t.id === activateTabForGroups[groupId];
+            });
+            if (filteredTabs.length > 0) {
+              // 最后激活的标签还在
+              swithToTab(activateTabForGroups[groupId]);
+            } else {
+              // 最后激活的标签不在了，那就激活该分组第一个标签
+              swithToTab(tabs[0].id);
+            }
+          } else {
+            // 当前分组没有激活过的标签，就激活第一个
+            swithToTab(tabs[0].id);
+          }
         } else {
+          // 指向的是普通标签
           const id = Number(tab.parentNode.id.replace(/^\D+/g, ""));
-          chrome.tabs.update(id, { active: true, highlighted: true });
+          swithToTab(id);
         }
       }, delay);
     }
