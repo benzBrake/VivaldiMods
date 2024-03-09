0dbf3b89aa7fec708b20b936030caca2349ad020
Ryan Lieu <github-benzBrake@woai.ru>
2024-03-09 19:45:33 +0800

    fix: (JS) rightClickTabToClose.ac.js 不兼容“关闭最后一个标签页不关闭窗口”的问题

--- chrome/userChromeJS/rightClickTabToClose.ac.js
+++ chrome/userChromeJS/rightClickTabToClose.ac.js
@@ -6,9 +6,14 @@
 // @version         20240309
 // @charset         UTF-8
 // @homepageURL     https://github.com/benzBrake/VivaldiMods/tree/main/chrome/userChromeJS
-// @note            20240309 支持关闭标签分组
+// @note            20240309 支持关闭标签分组，修复不兼容关闭最后一个标签页不关闭窗口
 // ==/UserScript==
 (function closeTabs(tabContainer, tabSubContainer) {
+    async function getTabById(id) {
+        let tabs = await chrome.tabs.query({ currentWindow: true });
+        let [tab] = tabs.filter((tab) => tab.id === id) || [null];
+        return tab;
+    }
     // 绑定事件
     function bindEvent(tabContainer) {
         tabContainer.addEventListener("contextmenu", async (e) => {
@@ -20,19 +25,27 @@
                 ) {
                     e.stopPropagation();
                     e.preventDefault();
-                    if (tab.parentNode.classList.contains("is-substack")) {
-                        const groupId = tab.id.replace(/^tab-/g, "");
-                        let tabs = await chrome.tabs.query({ currentWindow: true });
-                        tabs = tabs.filter(t => {
-                            return JSON.parse(t.vivExtData).group == groupId;
-                        });
-                        console.log(tabs);
-                        if (tabs.length > 0) {
-                            chrome.tabs.remove(tabs.map(t => t.id));
-                        }
+                    let keepWindowOpenAfterLastTabClosed = await vivaldi.prefs.get('vivaldi.tabs.never_close_last');
+                    let currentId = tab.id.replace(/^tab-/g, "");
+                    let homePage = await vivaldi.prefs.get('vivaldi.homepage');
+                    let tabs = await chrome.tabs.query({ currentWindow: true });
+                    let isLastTabEl = tab.parentNode.children.length === 1; // 是否为最后一个 Tab 
+                    if (keepWindowOpenAfterLastTabClosed && isLastTabEl) {
+                        // 创建新标签页，并删除此标签页以外的标签页，包括标签栈
+                        const lastTab = await chrome.tabs.create({ url: homePage });
+                        await chrome.tabs.remove(tabs.filter(t => t.id !== lastTab.id).map(t => t.id));
                     } else {
-                        const id = Number(tab.id.replace(/^\D+/g, ""));
-                        chrome.tabs.remove(id);
+                        if (tab.parentNode.classList.contains("is-substack")) {
+                            
+                            tabs = tabs.filter(t => {
+                                return JSON.parse(t.vivExtData).group == currentId;
+                            });
+                            if (tabs.length > 0) {
+                                chrome.tabs.remove(tabs.map(t => t.id));
+                            }
+                        } else {
+                            chrome.tabs.remove(Number(currentId));
+                        }
                     }
                 }
             }
