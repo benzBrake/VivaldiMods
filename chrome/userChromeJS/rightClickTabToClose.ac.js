82d1bb86ebcb7d1a0fabff2aeb0bc03b031090d5
Ryan Lieu <github-benzBrake@woai.ru>
2024-10-23 19:42:48 +0800

    feat: (JS) rightClickTabToClose.ac.js 跟随 userChrome.js 更新

--- chrome/userChromeJS/rightClickTabToClose.ac.js
+++ chrome/userChromeJS/rightClickTabToClose.ac.js
@@ -3,66 +3,80 @@
 // @description     右键关闭标签页
 // @license         MIT License
 // @compatibility   Vivaldi 6
-// @version         20240309
+// @version         20241023
 // @charset         UTF-8
 // @homepageURL     https://github.com/benzBrake/VivaldiMods/tree/main/chrome/userChromeJS
+// @note            20241023 跟随 userChrome.js 更新
 // @note            20240309 支持关闭标签分组，修复不兼容关闭最后一个标签页不关闭窗口
 // ==/UserScript==
-(function closeTabs(tabContainer, tabSubContainer) {
-    // 绑定事件
-    function bindEvent(tabContainer) {
-        tabContainer.addEventListener("contextmenu", async (e) => {
-            let tab = e.target.closest('.tab');
-            if (tab) {
-                if (
-                    !e.shiftKey &&
-                    !e.ctrlKey
-                ) {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    let keepWindowOpenAfterLastTabClosed = await vivaldi.prefs.get('vivaldi.tabs.never_close_last');
-                    let currentId = tab.id.replace(/^tab-/g, "");
-                    let homePage = await vivaldi.prefs.get('vivaldi.homepage');
-                    let tabs = await chrome.tabs.query({ currentWindow: true });
-                    let isLastTabEl = tab.closest('.tab-strip').querySelectorAll(':scope>span').length === 1; // 是否为最后一个 Tab
-                    if (keepWindowOpenAfterLastTabClosed && isLastTabEl) {
-                        // 创建新标签页，并删除此标签页以外的标签页，包括标签栈
-                        const lastTab = await chrome.tabs.create({ url: homePage });
-                        await chrome.tabs.remove(tabs.filter(t => t.id !== lastTab.id).map(t => t.id));
-                    } else {
-                        if (tab.parentNode.classList.contains("is-substack")) {
-                            tabs = tabs.filter(t => {
-                                return JSON.parse(t.vivExtData).group == currentId;
-                            });
-                            if (tabs.length > 0) {
-                                chrome.tabs.remove(tabs.map(t => t.id));
-                            }
-                        } else {
-                            chrome.tabs.remove(Number(currentId));
-                        }
-                    }
-                }
-            }
+(function () {
+    async function getTabs (options = {}, filter) {
+        Object.assign(options, {
+            currentWindow: true
         });
+        tabs = await chrome.tabs.query(options);
+        tabs = tabs.filter(tab => {
+            let extData = JSON.parse(tab.vivExtData);
+            return !("panelId" in extData);
+        });
+        if (typeof filter === "function") tabs = tabs.filter(filter);
+        return tabs;
     }
 
-    bindEvent(tabContainer);
-    if (tabSubContainer) {
-        bindEvent(tabSubContainer);
+    async function switchToTab (id) {
+        return await chrome.tabs.update(id, { active: true, highlighted: true });
     }
+
+    async function closeTab (event) {
+        if (
+            event.shiftKey ||
+            event.ctrlKey
+        ) return;
+        const target = $(event.target);
+        const tab = target.closest('.tab');
+        const tabPosition = tab.closest('.tab-position');
+        if (!tab.length || !tabPosition.length) return;
+        event.stopPropagation();
+        event.preventDefault();
+        const currentId = tab.get(0).id.replace(/^tab-/g, "");
+        let toBeRemoved = [];
+        if (tabPosition.hasClass('is-substack')) {
+            let tabs = await getTabs();
+            console.log(tabs);
+            tabs = tabs.filter(t =>
+                JSON.parse(t.vivExtData).group == currentId
+            );
+            toBeRemoved = tabs.map(t => t.id);
+        } else {
+            toBeRemoved = [Number(currentId)];
+        }
+        let existsTabs = await getTabs();
+        let keepWindowOpenAfterLastTabClosed = await vivaldi.prefs.get('vivaldi.tabs.never_close_last');
+        if (existsTabs.length === toBeRemoved.length && keepWindowOpenAfterLastTabClosed) {
+            let homePage = await vivaldi.prefs.get('vivaldi.homepage');
+            await chrome.tabs.create({ url: homePage });
+        }
+        await chrome.tabs.remove(toBeRemoved);
+        let activateTab = await getTabs({ active: true });
+        if (!activateTab.length) {
+            let existsTabs = await getTabs();
+            await switchToTab(existsTabs[0].id);
+        }
+    }
+
+    $('#tabs-container .tab-strip').on('contextmenu', closeTab);
+    $('#tabs-subcontainer .tab-strip').on('contextmenu', closeTab);
 
     // 调整标签位置后重新绑定事件
-    let appendChild = Element.prototype.appendChild;
-    Element.prototype.appendChild = function () {
-        if (
-            arguments[0].tagName === "DIV" &&
-            arguments[0].classList.contains("tabbar-wrapper")
+    $(document).on('appendChild', function (event) {
+        const insertElement = event.detail[0];
+        if (!insertElement) return;
+        if (insertElement.tagName !== "DIV") return;
+        if (insertElement.classList.contains("tabbar-wrapper")
         ) {
-            bindEvent(arguments[0].querySelector('#tabs-container .tab-strip'));
-        } else if (arguments[0].tagName === "DIV" &&
-            arguments[0].id === "tabs-subcontainer") {
-            bindEvent(arguments[0].querySelector('.tab-strip'));
+            $('#tabs-container .tab-strip', insertElement).on('contextmenu', closeTab);
+        } else if (insertElement.id === "tabs-subcontainer") {
+            $('.tab-strip', insertElement).on('contextmenu', closeTab);
         }
-        return appendChild.apply(this, arguments);
-    };
-})(document.querySelector('#tabs-container .tab-strip'), document.querySelector('#tabs-subcontainer .tab-strip'));
+    });
+})();
